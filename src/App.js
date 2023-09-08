39e18278f14d7f2955490bd2a52213e6f2dfb4dd
P sudarshan <sudarshanp961@gmail.com>
2023-09-08 18:09:37 +0530

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PdfExport from './components/PdfExport';
+
+function App() {
+  return (
+    <div>
+      <h1>POC for Export PDF</h1>
+      <PdfExport />
+    </div>
+  );
+}
+
+export default App;
